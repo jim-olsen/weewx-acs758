a1687f0fa42f6f1202aa98d1ad1a1cff73b39d64
Jim Olsen <jim.olsen@modelop.com>
2020-08-24 09:13:32 -0600

    Updating colors, adding live update text and changing to different intervals for graph and text.  Maxing out to 24 hours worth of graph data

--- src/dash-app/app.py
+++ src/dash-app/app.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,64 +6,86 @@ import plotly
 import requests
 from dash.dependencies import Input, Output
 
+main_div_style = {
+	'backgroundColor': '#111111',
+	'color': '#fca503',
+	'height': '100vh'
+}
+
+divStyle = {
+	'backgroundColor': '#111111',
+	'color': '#fca503'
+}
+
+graphStyle = {
+	'plot_bgcolor': '#111111',
+	'paper_bgcolor': '#111111',
+	'font': {
+		'color': '#fca503'
+	}
+}
+
 app = dash.Dash(__name__)
 app.layout = html.Div(
-	html.Div([
-		html.H4('Cabin Electric Load'),
+	style = divStyle,
+	children = [
+	html.Div(
+		style = main_div_style,
+		children = [
+		html.H2('Cabin Electric Load'),
 		html.Div(id='live-update-text'),
 		dcc.Graph(id='live-update-graph'),
 		dcc.Interval(
-			id='interval-component',
-			interval=1 * 5000,  # in milliseconds
+			id='text-interval-component',
+			interval=5000,  # in milliseconds
 			n_intervals=0
-		)
-	])
+		),
+		dcc.Interval(
+			id='graph-interval-component',
+			interval=60000,  # in milliseconds
+			n_intervals=0
+			)
+		])]
 )
 
 
-@app.callback(Output('live-update-text', 'children'), [Input('interval-component', 'n_intervals')])
-def update_graph_metrics(n):
+@app.callback(Output('live-update-text', 'children'), [Input('text-interval-component', 'n_intervals')])
+def update_text_metrics(n):
 
 	resp = requests.get('http://10.0.10.128/A0')
+	current_load = 'N/A'
 	if resp.status_code == 200:
-		json_str = resp.json()
+		resp_dict = resp.json()
 		print('Received: ')
-		print(json_str)
-		vals = json.loads(json_str)
-		graphData['time'].append(datetime.datetime.now())
-		graphData['cabinload'].append(vals['A0'])
+		print(resp_dict)
+		current_load = '{0:.2f}'.format(resp_dict['A0'])
 	else:
 		print('Failed to communicate to arduino: ' + resp.status_code)
 
-	[Input('interval-component', 'n_intervals')]
+	return html.Span('Current Load: ' + current_load)
 
-
-def update_metrics(n):
-	resp = requests.get('http://10.0.10.128/A0')
-	if resp.status_code == 200:
-		json_str = resp.json()
-		print('Received: ')
-		print(json_str)
-		vals = json.loads(json_str)
-		graphData['time'].append(datetime.datetime.now())
-		graphData['cabinload'].append(vals['A0'])
-		style = {'padding': '2px', 'fontSize': '16px'}
-		return [
-			html.Span('Cabin Load: {}'.format(vals['A0']), style=style)
-		]
-	else:
-		print('Failed to communicate to arduino: ' + resp.status_code)
-
-	return []
-
-
-@app.callback(Output('live-update-graph', 'figure'), [Input('interval-component', 'n_intervals')])
+@app.callback(Output('live-update-graph', 'figure'), [Input('graph-interval-component', 'n_intervals')])
 def update_graph_live(n):
 	fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-	fig['layout']['margin'] = {'l': 30, 'r': 10, 'b': 30, 't': 10}
-	fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
-	fig.append_trace({'x': graphData['time'], 'y': graphData['cabinload'], 'name': 'Load', 'mode': 'lines+markers',
-						'type': 'scatter'}, 1, 1)
+	resp = requests.get('http://10.0.10.128/A0')
+	current_load = 'N/A'
+	if resp.status_code == 200:
+		resp_dict = resp.json()
+		print('Received: ')
+		print(resp_dict)
+		graphData['time'].append(datetime.datetime.now())
+		graphData['cabinload'].append(resp_dict['A0'])
+		if (len(graphData) > 1440):
+			graphData['time'].pop(0)
+			graphData['cabinload'].pop(0)
+
+		fig['layout'] = graphStyle;
+		fig['layout']['margin'] = {'l': 30, 'r': 10, 'b': 30, 't': 10}
+		fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
+		fig.append_trace({'x': graphData['time'], 'y': graphData['cabinload'], 'name': 'Load', 'mode': 'lines+markers',
+					  'type': 'scatter'}, 1, 1)
+	else:
+		print('Failed to communicate to arduino: ' + resp.status_code)
 	return fig
 
 
@@ -72,7 +93,6 @@ def main():
 	global graphData
 	graphData = {'cabinload': [], 'time': []}
 	app.run_server(debug=True, host='0.0.0.0')
-
 
 if __name__ == '__main__':
 	main()
